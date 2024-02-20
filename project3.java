4fe2eb810e45096bddcc0a07cdde8d9c8f802d98
Dennis Do <89269108+DennisD0@users.noreply.github.com>
2024-02-20 15:13:33 -0500

    Create project3.java

---
+++ project3.java
@@ -0,0 +1,22 @@
+/**
+ * The Project3 class contains the main method that creates the GUI for the RomanNumeralGUI class.
+ */
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+
+public class Project3 {
+
+	/**
+	 * The main method creates the GUI for the RomanNumeralGUI class.
+	 *
+	 * @param args an array of command-line arguments for the program (not used in
+	 *             this program)
+	 * @throws FileNotFoundException if the file specified in the program is not
+	 *                               found
+	 */
+	public static void main(String[] args) throws FileNotFoundException {
+		// create the GUI using the RomanNumeralGUI class
+		RomanNumeralGUI.createGUI();
+	}
+}
